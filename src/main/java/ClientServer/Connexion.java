09d24443aea35612246a574bddbfd3240181f184
Cocux <clem.mercier94@gmail.com>
2020-05-30 12:21:26 +0200

    Requests

--- src/main/java/ClientServer/Connexion.java
+++ src/main/java/ClientServer/Connexion.java
@@ -10,18 +10,18 @@ import java.net.Socket;
 
 public class Connexion implements Runnable{
 
-    private Socket connexion = null;
     private BufferedInputStream reader = null;
+    private Socket connexion = null;
     private User user;
     private JTextArea textArea;
-    private JComboBox contact;
-    private String response;
+    private JComboBox contacts;
+    private String reponse;
     
     public Connexion(Socket connexion, User user, JTextArea textArea, JComboBox contact){
         this.connexion = connexion;
         this.user = user;
         this.textArea = textArea;
-        this.contact = contact;
+        this.contacts = contact;
     }
 
 
@@ -34,12 +34,59 @@ public class Connexion implements Runnable{
         return response;
     }
 
-    @Override
-    public void run() {
+    public void run(){
+        while(true) {
+            try {
+                reader = new BufferedInputStream(connexion.getInputStream());
+                reponse = "";
+                String response = read();
+                Root Json = null;
+                String json = "";
+                String[] tabResponse = response.split(" ");
+                RequestCode Code = RequestCode.values()[Integer.parseInt(tabResponse[0])-1];
+                response = tabResponse[1];
+                switch (Code) {
+                    case DECONNEXION:
+                        break;
+                    case SEND_MESSAGE:
+                        String expediteur = tabResponse[2];
+                        if(expediteur.equals(contacts.getSelectedItem()))
+                        {
+                            reponse = response;
+                            textArea.append(response);
+                        }
 
+                        break;
+                    case CHANGE_PASSWORD:
+                        reponse = response;
+                        break;
+                    case CHANGE_LOGIN:
+                        reponse = response;
+                        break;
+                    case ADD:
+                        reponse = response;
+                        break;
+                    case CREATE_GROUP:
+                        break;
+                    case HISTORY:
+                        reponse = response;
+                        break;
+                }
+
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+            try {
+                Thread.currentThread().sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public String getResponse(){
-        return this.response;
+        return this.reponse;
     }
 }
